bc65943779a70dcf727cc20a23de81ed4af2fe87
caioandrade2311 <96748628+caioandrade2311@users.noreply.github.com>
2022-01-20 01:30:10 -0300

    Add files via upload

---
+++ precache-manifest.5ff774199c887ec8c1bd11aa3e951dde.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd5907dd9f288952ff3d58934d5fb8dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "55fe8749bc48ad0a1d2f",
+    "url": "/static/css/main.7279dd61.chunk.css"
+  },
+  {
+    "revision": "a9de5d3ef81213ac6f37",
+    "url": "/static/js/2.8578f7a4.chunk.js"
+  },
+  {
+    "revision": "ad58a1d75bba2b1a094f6492c2b4b0ed",
+    "url": "/static/js/2.8578f7a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55fe8749bc48ad0a1d2f",
+    "url": "/static/js/main.e650abc5.chunk.js"
+  },
+  {
+    "revision": "dfe3d817a9ac0c05d2c9",
+    "url": "/static/js/runtime-main.84cfcdfc.js"
+  }
+]);
